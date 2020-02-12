c70f58cfbfc782aa9fba5e4eabfffcac7a3910c1
hkbeyond2 <50941712+hkbeyond2@users.noreply.github.com>
2020-02-12 14:06:45 +0800

    1
    1

--- src/main/java/com/SpringcloudProApp.java
+++ src/main/java/com/SpringcloudProApp.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class SpringcloudProApp {
-
+//1
     public static void main(String[] args) {
         SpringApplication.run(SpringcloudProApp.class, args);
     }
